776fb5da3067ec0227e33e19bad0f53b94019ad5
ItsPedrim <136517375+ItsPedrim@users.noreply.github.com>
2023-10-19 20:27:44 +0000

    Código Incompleto

--- Lab01-2023-1-main/src/main/App.java
+++ Lab01-2023-1-main/src/main/App.java
@@ -1,14 +1,48 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
-    }
+
+    public static int encontre(int[] arrayOrdenado, int num) {
+
+        int contador = 0;
+
+        while (contador < arrayOrdenado.length - 1) {
+                
+            if (num == arrayOrdenado[contador]) {
+                    
+                return 1;
+
+                }
+                
+            else {
+
+                continue;
+
+                }
+
+            contador += 1;
+        }
+        
+    return -1; 
+
+}
 
     public static int maior(int[] arrayDesordenado){
+        
         return -1;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
+        
         return 1;
     }
+
+    public static void main(String [] args) {
+
+        int[] array1 = new int[] {1, 2, 3, 4, 5};
+
+        System.out.println(encontre(array1, 3));
+
+    }
+    
 }
